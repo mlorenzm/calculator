674c1c5db4eb9bc0e431edf3634039473f636afe
mlorenzm <mlorenzomohamed@gmail.com>
2023-02-08 20:20:30 +0000

    Add major styling/components

---
+++ app.js
@@ -0,0 +1,19 @@
+const add = (a,b) => {
+    return a + b;
+};
+
+const subtract = (a,b) => {
+    return a - b;
+};
+
+const multiply = (a,b) => {
+    return a * b;
+};
+
+const divide = (a,b) => {
+    if (b == 0){
+        return 'idk';
+    }
+    return a / b;
+}
+
