bb2cd58b3424609a70b20e66337d2b10da7434b0
mlorenzm <mlorenzomohamed@gmail.com>
2023-03-01 21:08:32 +0000

    Fix screen bug (it went to 0 between operands)

--- app.js
+++ app.js
@@ -43,17 +43,19 @@ let previousNumber;
 let lastNumber;
 let operator;
 let equal = false;
-let dot;
+let ans = false;
+
 
 numberButtons.forEach(item =>{
     item.addEventListener('click', e =>{
-        if(screen.textContent == 0){
+        if(screen.textContent == '0'){
             screen.textContent = item.textContent;
+        } else if(previousNumber && !ans){
+            screen.textContent = item.textContent;
+            ans = true;
         }else{
             screen.textContent += item.textContent
         }
-
-        // something if the operator has been used
     })
 });
 
@@ -61,21 +63,21 @@ operatorButtons.forEach(item =>{
     item.addEventListener('click', e =>{
         if (previousNumber){
             currentNumber = parseFloat(screen.textContent);
+            console.log(`current number is ${currentNumber}`)
             previousNumber = operate(currentNumber, operator, previousNumber);
             screen.textContent = previousNumber;
+            ans = false;
         } else{
             previousNumber = parseFloat(screen.textContent);
         }
-        screen.textContent = '0'
+        screen.textContent = previousNumber
         operator = item.textContent;
-        dot = false;
     })
 });
 
 dotButton.addEventListener('click', e=>{
     if (!screen.textContent.includes('.')){
         screen.textContent += '.';
-    
     }
 })
 function clear(display){
@@ -84,13 +86,14 @@ function clear(display){
     lastNumber = undefined;
     operator = undefined;
     equal = false;
-    dot = false;
+    ans = false;
 }
 
 
 equalButton.addEventListener('click', e =>{
     if(operator == undefined){
-        screen.textContent = 'Error';}
+       // do nothing, but don't collapse display
+    }
     else if (!equal){
         lastNumber = parseFloat(screen.textContent);
         equal = true;
